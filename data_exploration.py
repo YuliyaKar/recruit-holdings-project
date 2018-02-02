7876f4bcda46a1af7d023a5cb2052bce64293e55
YuliyaK <y.karanouskaya@gmail.com>
2018-02-02 11:42:12 +0100

    Initial commit

---
+++ data_exploration.py
@@ -0,0 +1,14 @@
+"""
+  Recruit Holdings Kaggle Competition.
+
+  Inspired by the following Kaggle kernels:
+    -
+    -
+    -
+
+  author: YuliyaK
+  date: 2/2/2018
+"""
+
+import pandas
+import matplotlib.pyplot as plt
