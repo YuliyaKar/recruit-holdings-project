d5a760f8f62488024dc8fcfb160c11460caa53f8
YuliyaK <y.karanouskaya@gmail.com>
2018-02-02 11:57:38 +0100

    Add function to load data

--- data_exploration.py
+++ data_exploration.py
@@ -10,5 +10,17 @@
   date: 2/2/2018
 """
 
-import pandas
+import pandas as pd
 import matplotlib.pyplot as plt
+
+def load_data():
+    data = {'air_reserve': pd.read_csv('data/air_reserve.csv'),
+            'air_store_info': pd.read_csv('data/air_store_info.csv'),
+            'air_visit_data': pd.read_csv('data/air_visit_data.csv'),
+            'date_info': pd.read_csv('data/date_info.csv'),
+            'hpg_reserve': pd.read_csv('data/hpg_reserve.csv'),
+            'hpg_store_info': pd.read_csv('data/hpg_store_info.csv'),
+            'store_id_relation': pd.read_csv('data/store_id_relation.csv'),
+            }
+
+    return data
